7a3985b538790ec173bdd5778ad47f60e226afef
Benjamin Thio <74221883+BenjaminThio@users.noreply.github.com>
2025-12-12 18:22:52 +0800

    Urgent fixes.

--- api/bot.ts
+++ api/bot.ts
@@ -1,3 +1,13 @@
+
+import { Context } from 'grammy';
+import { Snake, SnakeCallback } from '@src/commands/snake';
+import { TicTacToe, TicTacToeCallback } from '@src/commands/tic_tac_toe';
+import { Temperature, Weather } from '@src/commands/weather';
+import { ChessCallback, PlayChess } from '@src/commands/chess';
+import { Sokoban, SokobanCallback } from '@src/commands/sokoban';
+import { Dict } from '@src/commands/dictionary';
+import { test, Time } from '@src/commands/datetime';
+import { Calculator, CalculatorCallback } from '@src/commands/calculator';
 import { webhookCallback, GrammyError, HttpError } from 'grammy'
 import { VercelRequest, VercelResponse } from "@vercel/node"
 import { bot } from '@src/bot';
@@ -15,6 +25,47 @@ bot.catch((err) => {
         console.error("Unknown error: ", e);
     }
 })
+
+bot.command('calc', (ctx) => {
+	Calculator(ctx);
+});
+bot.command('date', (ctx) => {
+	test(ctx);
+});
+bot.command('time', (ctx) => {
+	Time(ctx);
+});
+bot.command('d', (ctx) => {
+	Dict(ctx);
+});
+bot.command('sokoban', (ctx: Context) => {
+	Sokoban(ctx);
+});
+bot.command('chess', (ctx: Context) => {
+	PlayChess(ctx);
+});
+bot.command('snake', (ctx: Context) => {
+	Snake(ctx);
+});
+bot.command('tic_tac_toe', (ctx: Context) => {
+	TicTacToe(ctx);
+});
+bot.command('weather', (ctx: Context) => {
+	Weather(ctx);
+});
+bot.command('temp', (ctx: Context) => {
+	Temperature(ctx);
+});
+bot.command('test', async (ctx) => {
+	await ctx.reply('Testing123');
+});
+
+SnakeCallback();
+TicTacToeCallback();
+ChessCallback();
+SokobanCallback();
+CalculatorCallback();
+
 
 export default async (req: VercelRequest, res: VercelResponse) => {
     console.log("Incoming Webhook Request.");
--- src/bot.ts
+++ src/bot.ts
@@ -1,55 +1,7 @@
-import { Context, Bot } from 'grammy';
-import { Snake, SnakeCallback } from './commands/snake';
-import { TicTacToe, TicTacToeCallback } from './commands/tic_tac_toe';
-import { Temperature, Weather } from './commands/weather';
-import { ChessCallback, PlayChess } from './commands/chess';
-import { Sokoban, SokobanCallback } from './commands/sokoban';
-import { Dict } from './commands/dictionary';
-import { test, Time } from './commands/datetime';
-import { Calculator, CalculatorCallback } from './commands/calculator';
+import { Bot } from 'grammy';
 
 const token = process.env.BOT_TOKEN;
 
 if (!token) throw new Error("TOKEN is unset");
 
 export const bot = new Bot(token);
-
-bot.command('calc', (ctx) => {
-	Calculator(ctx);
-});
-bot.command('date', (ctx) => {
-	test(ctx);
-});
-bot.command('time', (ctx) => {
-	Time(ctx);
-});
-bot.command('d', (ctx) => {
-	Dict(ctx);
-});
-bot.command('sokoban', (ctx: Context) => {
-	Sokoban(ctx);
-});
-bot.command('chess', (ctx: Context) => {
-	PlayChess(ctx);
-});
-bot.command('snake', (ctx: Context) => {
-	Snake(ctx);
-});
-bot.command('tic_tac_toe', (ctx: Context) => {
-	TicTacToe(ctx);
-});
-bot.command('weather', (ctx: Context) => {
-	Weather(ctx);
-});
-bot.command('temp', (ctx: Context) => {
-	Temperature(ctx);
-});
-bot.command('test', async (ctx) => {
-	await ctx.reply('Testing123');
-});
-
-SnakeCallback();
-TicTacToeCallback();
-ChessCallback();
-SokobanCallback();
-CalculatorCallback();
