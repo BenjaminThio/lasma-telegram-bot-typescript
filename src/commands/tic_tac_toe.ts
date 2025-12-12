9647c2376dfaba606cdec00ca2b931bbd60d36aa
Benjamin Thio <74221883+BenjaminThio@users.noreply.github.com>
2025-12-12 22:21:30 +0800

    Update tic_tac_toe.ts

--- src/commands/tic_tac_toe.ts
+++ src/commands/tic_tac_toe.ts
@@ -1,5 +1,5 @@
 import { Context, InlineKeyboard } from 'grammy';
-import { bot } from '@src/bot';
+import { bot } from '../bot';
 
 const numbers = ['1️⃣', '2️⃣', '3️⃣', '4️⃣', '5️⃣', '6️⃣', '7️⃣', '8️⃣', '9️⃣'];
 const board = ['#', '#', '#', '#', '#', '#', '#', '#', '#'];
